58b8603d65344075a75cb94dc8c20f7fbb52c3e5
barbaram27 <barbara.silvapdita924@pditabira.com>
2025-07-14 18:28:04 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,10 @@
+x = 5
+y = 2
+
+print(type(x))
+print(type(y))
+
+resultado= x / y
+
+print(resultado)
+print(type(resultado))
---
+++ modulo2/aula2_questao3.py
@@ -0,0 +1,8 @@
+v1 = 10
+v2 = 20
+valor = v1
+v1= v2
+v2= valor
+
+print(v1)
+print(v2)
