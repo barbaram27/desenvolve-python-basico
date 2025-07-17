8b134c9c89825493baa1a3bf52f018cfdecdea3c
barbaram27 <barbara.silvapdita924@pditabira.com>
2025-07-17 10:36:38 -0300

    Add files via upload

---
+++ modulo2/aula4_questao1.py
@@ -0,0 +1,7 @@
+comprimento = 25
+largura =  10
+preço_m2 = 2049.96
+area_m2 = comprimento * largura
+preço_total = preço_m2 * area_m2
+print(f"O terreno possui {area_m2}m2 e custa R${preço_total:,.2f}")
+
