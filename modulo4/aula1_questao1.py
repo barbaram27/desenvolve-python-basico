b01bd9a0ffa085c71e2818bbcbba4e8fb771a639
barbaram27 <barbara.silvapdita924@pditabira.com>
2025-07-17 13:03:14 -0300

    Add files via upload

---
+++ modulo4/aula1_questao1.py
@@ -0,0 +1,5 @@
+x = int(input("Digite um valor para x: "))
+
+if x > 5:
+    print("Maior que 5")
+print("Fim")
---
+++ modulo4/aula1_questao2.py
@@ -0,0 +1,8 @@
+n = int(input("Digite um valor para n: "))
+cont = 0
+
+while cont < n:
+    print(cont)
+    cont = cont + 1
+
+print("Fim")
